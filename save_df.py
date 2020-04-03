d185c2bfe1f21c388c0bebb3682bb486c513f771
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-04-02 19:23:27 -0700

    added download page, daily update, and added louisiana

--- save_df.py
+++ save_df.py
@@ -4,9 +4,15 @@ from glob import glob
 import pandas as pd
 import numpy as np
 
+from pandas import ExcelWriter
+
 def save_df(df, path, state):
 
     df = df.sort_values(by='County')
     path_save = f'{path}/{state}_master_file.csv'
     df.to_csv(path_save, index = False)
-        
+    
+    path_save2 = f'{path}/{state}_master_file.xls'
+    writer = ExcelWriter(path_save2)
+    df.to_excel(writer,'Sheet1', index=False)
+    writer.save()
