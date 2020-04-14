bc28aad82b66d8ceb554a808f24a770e7b90d393
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-04-13 19:54:19 -0700

    daily update

--- create_county_csv.py
+++ create_county_csv.py
@@ -28,20 +28,20 @@ def create_files(state, path, files):
                         if row in county_list:
                             break
                         else:
-                            if i[:-4] < "04-12-2020":
+                            # if i[:-4] < "04-12-2020":
                                 county_list.append(row)
-                            else:
-                                county_list_apr12update.append(row)
-    df = pd.DataFrame(county_list_apr12update)
+                            # else:
+                            #     county_list_apr12update.append(row)
+    # df = pd.DataFrame(county_list_apr12update)
     df1 = pd.DataFrame(county_list)
     df1.columns = ["FIPS", "County", "State", "Country", "Date", "lat", "lon", 
         "Confirmed", "Deaths", "Recovered", "Active", "Combined_Key"]
-    df.columns = ["State", "Country", "Date", "lat", "lon", "Confirmed", "Deaths", "Recovered", "Active","County","FIPS", 
-        "Combined_Key","Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"]
-    df = df[["FIPS","County","State", "Country","Date", "lat", "lon", "Confirmed", "Deaths", "Recovered", "Active", 
-        "Combined_Key","Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"]]
-    df = df.drop(["Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"], axis=1)
-    frames = [df1, df]
-    combined_df = pd.concat(frames)
+    # df.columns = ["State", "Country", "Date", "lat", "lon", "Confirmed", "Deaths", "Recovered", "Active","County","FIPS", 
+    #     "Combined_Key","Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"]
+    # df = df[["FIPS","County","State", "Country","Date", "lat", "lon", "Confirmed", "Deaths", "Recovered", "Active", 
+    #     "Combined_Key","Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"]]
+    # df = df.drop(["Incident_Rate","People_Tested", "People_Hospitalized", "UID", "ISO3"], axis=1)
+    # frames = [df1, df]
+    # combined_df = pd.concat(frames)
 
-    return(combined_df)
+    return(df1)
